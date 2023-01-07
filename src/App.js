6213be6909b33ad103c4dbc839bcf6f8550cc31f
eRTe RafałT <rtaraszka@poczta.fm>
2023-01-07 10:10:09 +0000

    Save Work

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <div className=''></div>
+          <div>
+            
+          </div>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
