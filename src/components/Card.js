2f9590a519ff6c4852a95633f620d8b88c2e443a
eRTe RafałT <rtaraszka@poczta.fm>
2023-02-18 17:05:54 +0000

    Listening to Events & Working witch Event Handlers Add button with event

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,11 @@
 import "./Card.css"
 
+// 1) Gdzie jest wywołana klasa Card
+    // w pliku Ekspenses.js.  
+//Jakie dane są do nie przekazne jako props?
+    // klasa css oraz elementy ExpenseItem
 function Card(props) {
     const classes= "card " + props.className;
     return <div className={classes}>{props.children}</div>
-//     const classes = 'card ' + props.className;
-  
-//   return <div className={classes}>{props.children}</div>;
 }
 export default Card;
