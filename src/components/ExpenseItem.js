35bc1f863643186e1625fd60885cadc9dc05f556
eRTe RafałT <rtaraszka@poczta.fm>
2023-02-25 15:43:14 +0000

    Working withstate/48

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,12 @@
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 import Card from './Card';
+import { useState } from 'react';
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
   const clickHandler = () => {
+    setTitle('Updated');
     console.log("Clicked!!!");
   }
 
@@ -14,7 +17,7 @@ function ExpenseItem(props) {
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
         <button onClick={clickHandler}>Change title</button>
       </div>
